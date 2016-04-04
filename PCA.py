5af8eea4ab5ee79859481729ac8294c6a6a95118
Andy He <he-x15@mails.tsinghua.edu.cn>
2016-04-04 13:43:18 +0800

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -15,12 +15,18 @@ from PIL import Image
 from utils import tile_raster_images
 
 def get_data(dpath):
+    '''
+    read in original data
+    '''
     fin = gzip.open(dpath, 'rb')
     train_set, test_set, cv_set = cPickle.load(fin)
     fin.close()
     return train_set[0]
 
 def preprocess(data):
+    '''
+    zero-centered the data
+    '''
     ndata = np.copy(data)
     ndata -= np.mean(ndata, axis=0)
     return ndata
@@ -28,13 +34,16 @@ def preprocess(data):
 def pca(ndata):
     Sigma = np.dot(ndata.T, ndata) / ndata.shape[0]
     U, S, V = svd(Sigma)
-    Urs = [U[:,:1].T,U[:,:5].T,U[:,:20].T,U[:,:100].T]
+    Urs = [U[:,:1].T,U[:,:5].T,U[:,:20].T,U[:,:100].T] # select first 1, 5, 20, 100 components.
     z = [np.dot(ndata, Urs[i].T) for i in range(4)]
     Ua = [np.dot(z[j], Urs[j]) for j in range(4)]
-    dwhite = np.dot(ndata, U) / np.sqrt(S + 1e-10)
+    dwhite = np.dot(ndata, U) / np.sqrt(S + 1e-10) #Whitening the data
     return Urs, z, Ua, dwhite
 
 def plot(data, Urs, Ua, dwhite):
+    '''
+    plot the pictures results.
+    '''
     image = Image.fromarray(
         tile_raster_images(
             X=data[:100],
